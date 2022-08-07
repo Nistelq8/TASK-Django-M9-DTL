2f9fefcdfdb6434be406395d60269f3af28e475d
Nistelq8 <aziz.almarzouq@gmail.com>
2022-08-07 19:10:06 +0300

    solved task

--- bareed/urls.py
+++ bareed/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from shops import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("ice-cream-detail/<int:ice_cream_id>", views.)
 ]
--- shops/views.py
+++ shops/views.py
@@ -1,3 +1,13 @@
+from multiprocessing import context
 from django.shortcuts import render
+from .models import IceCream
 
 # Create your views here.
+def get_ice_cream(request,IceCream_ID):
+    ice_cream = IceCream.objects.get(id=IceCream_ID)
+    context = { "ice_cream":{
+        'ice cream name': ice_cream.name,
+        'ice cream shop': ice_cream.shop,
+        'ice cream stock': ice_cream.stock,}
+    }
+    return render(request)
